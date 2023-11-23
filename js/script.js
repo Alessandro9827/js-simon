588d2539234be0593ab41540b9743a7a9d4e1df4
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-23 23:25:55 +0100

    js fix problem

--- js/script.js
+++ js/script.js
@@ -8,9 +8,6 @@ const minutesEl = document.querySelector('#minutes');
 const secondsEl = document.querySelector('#seconds');
 const panelEl = document.querySelector('.panel');
 
-//Data di capodanno
-const endDate = newDate("Gennaio 1, 2024 00:00:00");
-const endDateInMs = endDate.getTime();
 
 //Tabella MS (millisecondi)
 const secondInMs = 1000;
@@ -22,9 +19,15 @@ const counterTime = setInterval(timer, 1000);
 
 /*---- Funzioni ----*/
 function timer () {
+    
+    //Data di capodanno
+    const endDate = new Date("January 1 2024");
+    const endDateInMs = new Date();
+
     //oggi in ms
-    const newInMs = newDate().getTime;
-    const diff = endDateInMs = newInMs;
+    let newInMs = new Date().getTime;
+    let diff= Math.floor(endDate - endDateInMs);
+    
     if (diff > 0) {
         daysEl.innerHTML = Math.floor (diff / daysInMs);
         hoursEl.innerHTML = Math.floor ( (diff % daysInMs) /hoursInMs );
@@ -34,5 +37,5 @@ function timer () {
     else {
         clearInterval(timer);
         panelEl.innerHTML = "<h2> Happy New Year!</h2>";
-    }
+    }  
 }
